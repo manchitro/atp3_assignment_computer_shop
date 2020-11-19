0fa5f44aaab15d872b3b2f19d7b62893913416de
Sazid <initialsaremsu@gmail.com>
2020-11-19 13:32:32 +0600

    Created login and signup page for customer

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const signup = require("./controllers/signup");
+const login = require("./controllers/login");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use('/assets', express.static('assets'));
+app.use("/assets", express.static("assets"));
+
+app.use("/login", login);
+app.use("/signup", signup);
 
 app.get("/", (req, res) => {
   res.render("customer/index");
